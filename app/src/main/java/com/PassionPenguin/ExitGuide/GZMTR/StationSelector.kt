8ddbf21c50383d57acdce894ab9507a4afb3cce1
PassionPenguin <penguin.zhang@qq.com>
2020-01-25 01:55:04 +0800

    upd

--- app/src/main/java/com/PassionPenguin/ExitGuide/GZMTR/StationSelector.kt
+++ app/src/main/java/com/PassionPenguin/ExitGuide/GZMTR/StationSelector.kt
@@ -1,6 +1,5 @@
 package com.PassionPenguin.ExitGuide.GZMTR
 
-import android.app.Activity
 import android.content.Context
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
@@ -18,11 +17,11 @@ class StationSelector : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_station_selector)
 
-        val back = findViewById<Button>(R.id.cs_back)
-        back.setOnClickListener{
-            back.setBackgroundColor(0x44000000)
+//        val back = findViewById<Button>(R.id.cs_back)
+//        back.setOnClickListener{
+//            back.setBackgroundColor(0x44000000)
 //                startActivity(Intent(this@StationSelector,StationSelector::class.java))
-        }
+//        }
 val isDarkMode = this.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK===Configuration.UI_MODE_NIGHT_YES
 
         val webView = findViewById<WebView>(R.id.sswebview)
@@ -33,7 +32,7 @@ val isDarkMode = this.resources.configuration.uiMode and Configuration.UI_MODE_N
             }
             fun nextpage(num:Int,platnum:Int){
 station=arrayOf(num,platnum)
-                startActivity(Intent(this@StationSelector,StationInf::class.java))
+//                startActivity(Intent(this@StationSelector,StationInf::class.java))
             }
         }
         webView.getSettings().setJavaScriptEnabled(true);
@@ -41,8 +40,8 @@ station=arrayOf(num,platnum)
         webView.loadUrl("file:///android_asset/webview/choosestation.html")
     }
 
-    public fun getStatoinNum():Array<Int>{
-        return station
-    }
+//    public fun getStatoinNum():Array<Int>{
+//        return station
+//    }
 }
 
